98dd0003fa65e309ec4968aa55097febc764492c
TomiZ Dev <tomiz87dev@gmail.com>
2021-10-27 20:44:15 +0200

    Generating Numbers, Adding them to an array

--- Programming_TypeScript/generator/index.ts
+++ Programming_TypeScript/generator/index.ts
@@ -1,7 +1,35 @@
 const MAX_SAFE_INTEGER = 9007199254740991;
-let randomNum = randomNumber(1 , MAX_SAFE_INTEGER);
+let numArray: number[] = [];
+let randomNum;
+let caseinArray = false;
+
+while (numArray.length != 100) {
+    let randomNum = randomNumber(1 , MAX_SAFE_INTEGER);
+    let caseinArray = comparingNums(numArray.length, randomNum)
+    if (!caseinArray)
+        numArray.push(randomNum);
+        caseinArray = true;
+}
+numArray = numArray.sort((num1,num2) => num1 - num2);
+console.log(numArray.length)
+console.log(numArray)
+
 
 function randomNumber(min_num : number, max_num : number){
     return Math.floor(Math.random() * (max_num - min_num + 1)) + min_num;
 }
-console.log(randomNum);
+function comparingNums(numOfRepeats1: number, generatedNum: number) {
+    let comparing = false
+    for (let i: number = 0; i <= numOfRepeats1; i++)
+    {
+        if(generatedNum == numArray[i])
+        {
+           comparing = true;
+           break;
+        }
+    }
+    if (comparing)
+    return true;
+    if (!comparing)
+    return false;
+}
