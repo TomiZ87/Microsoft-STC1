5bc03ca6e924bb28e51a9b088b7b68f956533a43
TomiZ Dev <tomiz87dev@gmail.com>
2021-10-27 21:53:08 +0200

    Sorting, and filtering - parity

--- Programming_TypeScript/generator/src/index.ts
+++ Programming_TypeScript/generator/src/index.ts
@@ -1,25 +1,31 @@
 const MAX_SAFE_INTEGER = 9007199254740991;
 let numArray: number[] = [];
+let evenNumArray: number[] = [];
+let oddNumArray: number[] = [];
 let randomNum;
 let caseinArray = false;
 
 while (numArray.length != 100) {
     let randomNum = randomNumber(1 , MAX_SAFE_INTEGER);
-    let caseinArray = comparingNums(numArray.length, randomNum)
+    let caseinArray = comparingNums(numArray.length, randomNum);
     if (!caseinArray)
         numArray.push(randomNum);
         caseinArray = true;
 }
-numArray = numArray.sort((num1,num2) => num1 - num2);
-console.log(numArray.length)
-console.log(numArray)
 
+numArray = numArray.sort((num1,num2) => num1 - num2);
+let allNums: string = numArray.toString();
+console.log("Generated numbers: " + allNums)
+parityNum(numArray, 100, 0, evenNumArray)
+console.log(evenNumArray);
+parityNum(numArray, 100, 1, oddNumArray)
+console.log(oddNumArray);
 
 function randomNumber(min_num : number, max_num : number){
     return Math.floor(Math.random() * (max_num - min_num + 1)) + min_num;
 }
 function comparingNums(numOfRepeats1: number, generatedNum: number) {
-    let comparing = false
+    let comparing = false;
     for (let i: number = 0; i <= numOfRepeats1; i++)
     {
         if(generatedNum == numArray[i])
@@ -32,4 +38,15 @@ function comparingNums(numOfRepeats1: number, generatedNum: number) {
     return true;
     if (!comparing)
     return false;
+}
+
+function parityNum(fromArray: number[], numOfRepeats1: number, remainder: number, resultArray: number[]) {
+    for (let i: number = 0; i <= numOfRepeats1; i++)
+    {
+      if(fromArray[i] % 2 == remainder)
+      {
+        resultArray.push(fromArray[i]);
+      }  
+    }
+    return resultArray;
 }
