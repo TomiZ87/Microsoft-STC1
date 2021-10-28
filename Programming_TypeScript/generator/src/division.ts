8f5fb6b113cc36321430d95412ac0024e664b654
TomiZ Dev <tomiz87dev@gmail.com>
2021-10-28 22:32:57 +0200

    Comments on generator, final touches

--- Programming_TypeScript/generator/src/division.ts
+++ Programming_TypeScript/generator/src/division.ts
@@ -1,4 +1,17 @@
+/**
+ * Class witch constain the funtions to be used in another file/the prototype functions
+ */
 class division {
+    /**
+     * The function filters the numbers from an array if they are divisible by certain numbers with or without remainder and adds them to the chosen array
+     * If the number is divisible by the chosen divider and has the certain remainder, then is added to the chosen resultArray
+     * @param fromArray - the array from which are the initial values filtered
+     * @param resultArray - array to where will be the filtered values added
+     * @param numOfRepeats - the number of how many times has the loop repeat, usually it is the lenght of a fromArray (fromArray.lenght)
+     * @param remainder - the number determine which remainder (how many has to remain) has to result by dividing the number (divider)
+     * @param divider - the number by which will be the values divided (tested for)
+     * @returns 
+     */
     public static divisionNum(fromArray: number[], resultArray: number[], numOfRepeats: number, remainder: number, divider: number) {
         for (let i: number = 0; i <= numOfRepeats; i++)
         {
@@ -9,9 +22,13 @@ class division {
         }
         return resultArray;
     }
-    public static NumToString(resultArray: number[]) {
-        return resultArray.toString();
-    }
+    /**
+     * The function filters the prime numbers from an array and adds them to the resultArray
+     * @param fromArray - the array from which are the initial values filtered
+     * @param resultArray - array to where will be the filtered values added
+     * @param numOfRepeats  - the number of how many times has the loop repeat, usually it is the lenght of a fromArray (fromArray.lenght)
+     * @returns 
+     */
     public static primeNumbers (fromArray:number[], resultArray: number[], numOfRepeats: number)
     { 
     for (let i: number = 0; i <= numOfRepeats; i++)
@@ -31,4 +48,6 @@ class division {
     return resultArray;
     }
 }
+
+/** Exports the @class, so it can be used in another file in the project */
 export default division;
--- Programming_TypeScript/generator/src/types.ts
+++ Programming_TypeScript/generator/src/types.ts
@@ -1,6 +1,7 @@
+/** @type that contains prototype of one boolean and number variable */
 type nums = {
     MAX_SAFE_INTEGER: number;
     caseinArray: boolean;
 }
-
+/** Exports the @type, so it can be used in another file in the project */
 export default nums;
