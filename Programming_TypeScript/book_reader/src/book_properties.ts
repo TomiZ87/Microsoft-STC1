05007581ff5a756a60b53797b765151e63853ff8
TomiZ Dev <tomiz87dev@gmail.com>
2021-11-10 19:13:07 +0100

    Adding some stuff to framework

---
+++ Programming_TypeScript/book_reader/src/book_properties.ts
@@ -0,0 +1,16 @@
+export type Book = {
+    id: number
+    name: string
+    author: string[]
+    genre: string[]
+    published: number
+    publisher: string
+    pages: number
+}
+
+export type Book_Short = {
+    id: number
+    name: string
+    author: string[]
+    genre: string[]
+}
--- Programming_TypeScript/book_reader/src/index.ts
+++ Programming_TypeScript/book_reader/src/index.ts
@@ -1,10 +1,30 @@
-import express from "express"
-import cors from "cors"
-import bodyParser, { json, urlencoded} from "body-parser"
-import http from "http"
-import fs from "fs"
+import express from "express";
+import cors from "cors";
+import bodyParser, { json, urlencoded} from "body-parser";
+import http from "http";
+import fs from "fs";
+import {Book} from "./book_properties";
 
-const importedFile = fs.readFileSync("books.json")
-const myJSON = JSON.parse(importedFile.toString())
+let app
 
-console.log(myJSON)
+function createserver() {
+    app = express()
+    app.use(cors())
+    app.use(json())
+    app.use(urlencoded({extended: false}))
+
+    http.createServer(app).listen(3000, () =>{
+        console.log("Running server on port 3000")
+    })
+
+    app.get("/api/library/book/:id/info", (req, res) => {
+        const importedFile = fs.readFileSync("src/Input/books.json")
+        const myJSON: Book[] = JSON.parse(importedFile.toString())
+        res.json({myJSON})
+    })
+}
+createserver()
+
+
+
+
