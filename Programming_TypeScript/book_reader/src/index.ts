93a5f9285a15cbde5ad0ea77b403bac7fde11c87
TomiZ Dev <tomiz87dev@gmail.com>
2021-11-24 21:26:44 +0100

    Added put and delete methods

--- Programming_TypeScript/book_reader/src/index.ts
+++ Programming_TypeScript/book_reader/src/index.ts
@@ -5,16 +5,20 @@ import http from "http";
 import fs from "fs";
 import {Book, Book_Short} from "./book_properties";
 
-let app
-const importedFile = fs.readFileSync("src/Input/books.json")
-const myJSON: Book[] = JSON.parse(importedFile.toString())
+let app = express()
+const myJSON: Book[] = JSON.parse(fs.readFileSync(("src/Input/books.json")).toString())
 
 let myMap = new Map()
 myJSON.forEach((book: Book_Short) => myMap.set(book.id,<Book_Short> book))
 
 console.log(myJSON)
+
+function updateMap() {
+    const myJSON: Book[] = JSON.parse(fs.readFileSync(("src/Input/books.json")).toString())
+    let myMap = new Map()
+    myJSON.forEach((book: Book_Short) => myMap.set(book.id,<Book_Short> book))
+}
 function createserver() {
-    app = express()
     app.use(cors())
     app.use(json())
     app.use(urlencoded({extended: false}))
@@ -24,12 +28,12 @@ function createserver() {
     })
 
     app.get("/api/library/book/:id/info", (req, res) => {
+        updateMap()
         const id = req.params["id"]
         let tempid = parseInt(id)
-        let book = myMap.get(tempid)
-        console.log(tempid)
-        
         if (myMap.has(tempid)) {
+            let book = myMap.get(tempid)
+            console.log(tempid)
             let tempbookshort: Book_Short = {
                 id: book.id,
                 name: book.name,
@@ -43,19 +47,54 @@ function createserver() {
     })
 
     app.post("/api/library/book/:id/info", (req, res) => {
+        updateMap()
         const id = req.params["id"]
         let tempid = parseInt(id)
-        let book = myMap.get(tempid)
-        console.log(tempid)
         if (myMap.has(tempid)) {
+            let book = myMap.get(tempid)
+            console.log(tempid)
             res.json(book)
         } else {
             res.json({ id: "Error: NO book found on this id"})
         }
     })
+
+    app.put("/api/library/book/:id/add", (req, res) => {
+        updateMap();
+        const id = req.params["id"]
+        let tempid = parseInt(id)
+        if (myMap.has(tempid)) {
+            res.json({ id: "Error: Book found on this id"})
+        } else {
+            myJSON.push(
+            {
+                id: tempid,
+                name: req.body["name"],
+                author: req.body["author"],
+                genre: req.body["genre"],
+                published: req.body["published"],
+                publisher: req.body["publisher"],
+                origin_country: req.body["origin_country"],
+                pages: req.body["pages"]
+            }
+            )
+            fs.writeFileSync("src/Input/books.json", (JSON.stringify(myJSON, null, 2)));
+            res.json({ message: "The following book was successfully added!", id: myJSON[myJSON.length - 1]})
+        }
+    })
+    app.delete("/api/library/book/:id/delete", (req, res) => {
+        updateMap()
+        const id = req.params["id"]
+        let tempid = parseInt(id)
+        if (myMap.has(tempid)) {
+            let myJSON2 = myJSON.filter((book: Book) => book.id !== tempid);
+            fs.writeFileSync("src/Input/books.json", (JSON.stringify(myJSON2, null, 2)));
+            res.json({ message: "The following book was successfully deleted!", id: myJSON.filter((book: Book) => book.id === tempid)})
+            updateMap()
+        } else {
+            res.json({ id: "Error: No book found on this id"})
+            
+        }
+    })
 }
 createserver()
-
-
-
-
